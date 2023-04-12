98f5c750c01db77f09647b0207b39f67eef4a344
Nick <nickq4504@gmail.com>
2023-04-12 18:48:40 -0230

    -Changes made for the output to better comply with the example shown in the project PDF -The program now shows the updated money amount when the player loses -money.txt amount was reduced due to testing. The amount is now changed back to 100

--- Blackjack.py
+++ Blackjack.py
@@ -9,6 +9,7 @@ bet = 0
 
 def display_title():
     print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
 
     #Use a list for the suit, rank and point value
 
@@ -36,31 +37,33 @@ def card_deck():
 
     #Use a list of lists to store the dealer's hand and the player's hand.
 
-#Reset the player and dealer hands if the player selects "y" when asked if they want to play again
+    #Reset the player and dealer hands if the player selects "y" when asked if they want to play again
 def reset_hands():
     dealer_hand.clear()
     player_hand.clear()
 
 
 def get_bet(money):
+    print()
+    print(f"Money: {money:.1f}")
     while True:
         global bet
+        #Get the bet amount from the player
         bet = input("Bet amount: ")
+        print()
         try:
             bet = int(bet)
             if bet < 5 or bet > 1000:
-                # Make sure that the user can't make a bet greater than 1000 or less than 5.
+                #Make sure that the user can't make a bet greater than 1000 or less than 5.
                 raise ValueError("Invalid bet amount. Bet must be higher than 5 and less than 1000.")
         except ValueError as e:
             print(e)
         else:
-            # Make sure that the user can't make a bet that is greater than the money in the money.txt
+            #Make sure that the user can't make a bet that is greater than the money in the money.txt
             if bet > money:
                 print("Sorry, you don't have enough money to make that bet. Please try again.")
             else:
                 money -= bet
-                print(f"You just made a bet of: {bet}")
-                print(f"You now have ${money} remaining.")
                 write_money(money)
                 break
 
@@ -91,31 +94,36 @@ def play_game():
 
     deal_cards(deck)
 
-    print("YOUR CARDS: ")
-    for i in range(len(player_hand)):
-        print(str(player_hand[i][1]) + " of " + player_hand[i][0])
-        print()
 
     print("DEALER SHOW CARD: ")
     print(str(dealer_hand[0][1]) + " of " + dealer_hand[0][0])
+
     print()
+    print("YOUR CARDS: ")
+    for i in range(len(player_hand)):
+        print(str(player_hand[i][1]) + " of " + player_hand[i][0])
+
 
     #Get user's input for the bet
     #Player's turn
     while True:
         try:
+            print()
             choice = input("Hit or stand? (hit/stand): ")
             if choice.lower() == "hit":
                 card_selection = random.sample(deck, 1)
                 player_hand.append(card_selection[0])
                 deck.remove(card_selection[0])
+                print()
                 print("YOUR CARDS: ")
                 #Select a card for the player
                 for i in range(len(player_hand)):
                     print(str(player_hand[i][1]) + " of " + player_hand[i][0])
                 if sum([card[2] for card in player_hand]) > 21:
+                    print()
                     print("Sorry. You lose.")
                     write_money(money)
+                    print(f"Money: {money:.1f}")
                     return
             elif choice.lower() == "stand":
                 break
@@ -131,15 +139,18 @@ def play_game():
         card_selection = random.sample(deck, 1)
         dealer_hand.append(card_selection[0])
         deck.remove(card_selection[0])
+        print()
         print("DEALER CARDS: ")
         for i in range(len(dealer_hand)):
             print(str(dealer_hand[i][1]) + " of " + dealer_hand[i][0])
 
+
         if sum([card[2] for card in dealer_hand]) > 21:
             money += bet * 1.5
+            print()
             print("The dealer has busted! you win!")
-            print(f"money: {money}")
             write_money(money)
+            print(f"money: {money:.1f}")
             return
 
 #Determine who won. If the player wins, award the payout to the money.txt file
@@ -148,23 +159,27 @@ def determine_results(dealer_hand, player_hand, money):
     display_hand_points(dealer_hand, player_hand)
 
     if sum([card[2] for card in dealer_hand]) > sum([card[2] for card in player_hand]):
+        money -= bet
         print("Sorry. You lose.")
         print(money)
         print()
     elif sum([card[2] for card in dealer_hand]) == sum([card[2] for card in player_hand]):
+        money -= bet
         print("Tie!")
         print(money)
         print()
     else:
         money += bet * 1.5
+        print()
         print("Well done! you win!")
-        print(f"money: {money}")
         write_money(money)
+        print(f"money: {money:.1f}")
 
 def display_hand_points(dealer_hand, player_hand):
     #Determine score
     dealer_score = sum([card[2] for card in dealer_hand])
     player_score = sum([card[2] for card in player_hand])
+    print()
     print(f"YOUR POINTS: {player_score}")
     print(f"DEALER POINTS: {dealer_score}")
 
@@ -175,12 +190,14 @@ def main():
 
     play_again = "y"
     while play_again.lower() == "y":
+        print()
         play_again = input("Play again? (y/n)")
         if play_again.lower() == "y":
             reset_hands()
             play_game()
             display_hand_points(dealer_hand, player_hand)
 
+    print()
     print("Come back soon!")
     print("Bye!")
 
